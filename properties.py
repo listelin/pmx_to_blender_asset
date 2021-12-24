46cc032f90e756acbdb3734c6576451af35fd248
Your Name <you@example.com>
2021-12-24 23:44:14 +0900

    change default asset folder to ""

--- properties.py
+++ properties.py
@@ -3,11 +3,11 @@ from bpy.props import (StringProperty, FloatVectorProperty,BoolProperty)
 
 class PmxAssetizeProp(bpy.types.PropertyGroup):
 
-    source_pmx_folder : StringProperty(default='')
-    source_pmx : StringProperty(default='')
+    source_pmx_folder : StringProperty(default="")
+    source_pmx : StringProperty(default="")
     auto_asemble: BoolProperty(default = True)
     auto_smooth: BoolProperty(default = False)
-    asset_folder : StringProperty(default="C:\\bData\\asset3.0\\MMDtest\\")
+    asset_folder : StringProperty(default="")
 
     camera_location: FloatVectorProperty(name="Thumbnail auto camera location", default = (0, -0.75, 1.3))
     camera_rotation: FloatVectorProperty(name="Thumbnail auto camera rotation", default = (1.7, 0.0, 0.0))
