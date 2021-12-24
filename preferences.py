b52142d042d4fdcdc5ff875b55a38353907e0ae4
Your Name <you@example.com>
2021-12-24 20:37:55 +0900

    delete unsed preference

--- preferences.py
+++ preferences.py
@@ -6,24 +6,5 @@ from bpy.props import StringProperty
 class PmxAssetizePreferences(bpy.types.AddonPreferences):
     bl_idname = __package__
 
-
-    pmx_folder: StringProperty(
-        name='PMX Folder',
-        description='アセット化対象のPMXフォルダ',
-        default='',
-    )
-
-    pmx_blasset_folder: StringProperty(
-        name='Asset Folder',
-        description='アセットフォルダ',
-        default='',
-    )
-
     def draw(self, _context):
         layout: bpy.types.UILayout = self.layout  # pylint: disable=no-member
-
-        col = layout.box().column()
-        col.prop(self, 'pmx_blasset_folder')
-
-        col = layout.box().column()
-        col.prop(self, 'pmx_folder')
